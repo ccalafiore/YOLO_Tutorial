f74ee8ba2c8068baecdbff0e11b27d0c61df2ced
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2023-05-14 00:25:05 +0100

    multi threads to draw boxes in different images

--- step_1_yolo_object_detection_on_images.py
+++ step_1_yolo_object_detection_on_images.py
@@ -1,8 +1,8 @@
 
 import os
-import cv2
 from ultralytics import YOLO
 import utilities
+import calapy as cp
 
 
 root_script = __file__
@@ -22,7 +22,7 @@ dir_input_images = [os.path.join(root_input_images, name_i) for name_i in name_i
 
 root_yolos = os.path.join(root_data, 'yolos')
 name_yolo_versions = ['yolov8n.pt', 'yolov8s.pt', 'yolov8m.pt', 'yolov8l.pt', 'yolov8x.pt']
-v = 0
+v = 4
 name_yolo_version_v = name_yolo_versions[v]
 dir_yolo_version_v = os.path.join(root_yolos, name_yolo_version_v)
 
@@ -33,14 +33,17 @@ else:
 
 results_v = yolo_v(source=dir_input_images)
 
+boxes = [results_v[i].boxes for i in range(0, I, 1)]
+
 box_drawer = utilities.BoxDrawer(names=yolo_v.model.names, colors=None, threshold=(255 * .5))
 
 root_out_images = os.path.join(root_images, 'output_images', name_yolo_version_v.removesuffix('.pt'))
 os.makedirs(name=root_out_images, exist_ok=True)
 dir_out_images = [os.path.join(root_out_images, name_i) for name_i in name_input_images]
 
-out_images = [None for i in range(0, I, 1)]
-for i in range(0, I, 1):
-    out_images[i] = box_drawer(
-        boxes=results_v[i].boxes, input_image=dir_input_images[i], dir_out_image=dir_out_images[i])
+kwargs = [
+    {'boxes': boxes[i], 'input_image': dir_input_images[i], 'dir_out_image': dir_out_images[i]}
+    for i in range(0, I, 1)]
 
+multi_threads = cp.threading.MultiThreads(func=box_drawer, args=None, kwargs=kwargs, n_workers=None, names=None)
+out_images2 = multi_threads.run()
