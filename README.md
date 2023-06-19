5f879f5877ce36d8d8656d217eaee0b41c1e5db0
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2023-06-19 21:10:12 +0100

    detect video

--- README.md
+++ README.md
@@ -1,15 +1,39 @@
 # YOLO Tutorial for Object Detection 
 
-This is a guide to YOLO (You Only Look Once) model for object detection (OD).
+## Introduction
 
-All codes were coded on the built-in Python 3.10.6 of Ubuntu 22.04.2 LTS.
+This is a guide to YOLO (You Only Look Once) model for object detection (OD). 
+
+For more details watch the video at [https://youtu.be/WgPbbWmnXJ8](https://youtu.be/WgPbbWmnXJ8)
+
+## Setup
 
 The requirements.txt shows the required python packages to run all scripts in the project.
 
-For more details watch the video at https://youtu.be/WgPbbWmnXJ8
+## Step 0: Download YOLOs
+
+## Step 1: YOLO OD on Images
+
+![image_0](data/images/output_images/yolov8x/amici_dispersi.jpg)
+
+![image_1](data/images/output_images/yolov8x/bus_rapid_transit.jpg)
+
+![image_2](data/images/output_images/yolov8x/dinner.jpg)
+
+![image_3](data/images/output_images/yolov8x/dino_1.jpg)
+
+![image_4](data/images/output_images/yolov8x/people_cars_bicycles_1.jpg)
+
+![image_5](data/images/output_images/yolov8x/people_cars_bicycles_2.jpg)
+
+![image_7](data/images/output_images/yolov8x/trial.jpg)
 
 
+## Step 2: YOLO OD on live Camera Images
 
-### To do
-- step 0
+## Step 3: YOLO OD on Videos
+
+
+### Todo
+
 - step 2
--- step_2_yolo_object_detection_on_camera.py
+++ step_2_yolo_object_detection_on_camera.py
@@ -2,9 +2,121 @@
 # todo
 
 import os
+import math
+import time
+import keyboard
+import numpy as np
+import torch
 import utilities
 import calapy as cp
 import cv2
+
+
+def detect_video(
+        model, source, dir_out_video=None, show=False, size=None, fps=None,
+        do_track=False, do_count=False, timeout=None, quit_key=None):
+
+    if isinstance(model, str):
+        model = utilities.load_yolo(dir_model=model)
+
+    if isinstance(source, int):
+        cap = cv2.VideoCapture(index=source)
+    elif isinstance(source, str):
+        cap = cv2.VideoCapture(filename=source)
+    else:
+        raise TypeError('source')
+
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    if size is None:
+        size = w, h = cap.get(3), cap.get(4)
+    else:
+        if isinstance(size, int):
+            size = tuple([size, size])
+        elif isinstance(size, list):
+            size = tuple(size)
+        elif isinstance(size, tuple):
+            pass
+        elif isinstance(size, (np.ndarray, torch.Tensor)):
+            size = tuple(size.tolist())
+        else:
+            raise TypeError('size')
+
+        w, h = size
+        cap.set(3, w)
+        cap.set(4, h)
+
+    if fps is None:
+        fps = cap.get(cv2.CAP_PROP_FPS)
+    # if fps is None:
+    #     fps = math.floor(cap.get(cv2.CAP_PROP_FPS))
+    # elif isinstance(fps, int):
+    #     pass
+    # elif isinstance(fps, float):
+    #     fps = math.floor(fps)
+    # else:
+    #     raise TypeError('fps')
+
+    mspf = math.floor(1000 / fps)
+    spf = mspf / 1000
+
+    if timeout is None:
+        timeout = math.inf
+
+    if quit_key is None:
+        pass
+    elif isinstance(quit_key, (str, int)):
+        pass
+    else:
+        raise TypeError('quit_key')
+
+    if isinstance(source, int):
+        # start the model by processing one image
+        success, image = cap.read()
+        if success:
+            results = model(source=image, stream=False, verbose=False)
+
+    playing = True
+
+    timer = cp.clock.Timer()
+
+    while playing:
+
+        success, image = cap.read()
+
+        if success:
+
+            results = model(source=image, stream=False, verbose=False)
+
+            xyxy = results[0].boxes.xyxy
+            classes = results[0].boxes.cls
+            confidences = results[0].boxes.conf
+
+            image = box_drawer.draw_multi_boxes_in_single_image(
+                input_image=image, xyxy=xyxy, classes=classes, confidences=confidences, dir_out_image=None)
+
+            if show:
+                cv2.imshow(winname='Image', mat=image)
+                cv2.pollKey()
+                if keyboard.is_pressed(quit_key) or (timer.get_seconds() >= timeout):
+                    playing = False
+                else:
+                    # todo replace cv2.pollKey() with time.sleep() or cp.clock.wait()
+                    # cv2.waitKey(delay=mspf)
+                    time.sleep(spf)
+            else:
+                if keyboard.is_pressed(quit_key) or (timer.get_seconds() >= timeout):
+                    playing = False
+        else:
+            print("Can't receive frame (stream end?). Exiting ...")
+            playing = False
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return None
 
 
 root_script = __file__
@@ -12,61 +124,23 @@ root_project = os.path.dirname(root_script)
 root_data = os.path.join(root_project, 'data')
 
 root_yolos = os.path.join(root_data, 'yolos')
-name_yolo_versions = ['yolov8n.pt', 'yolov8s.pt', 'yolov8m.pt', 'yolov8l.pt', 'yolov8x.pt']
-v = 1
+name_yolo_versions = ['yolov8n', 'yolov8s', 'yolov8m', 'yolov8l', 'yolov8x']
+v = 0
 name_yolo_version_v = name_yolo_versions[v]
-dir_yolo_version_v = os.path.join(root_yolos, name_yolo_version_v)
+dir_yolo_version_v = os.path.join(root_yolos, name_yolo_version_v + '.pt')
 
-if os.path.exists(dir_yolo_version_v):
-    yolo_v = utilities.load_yolo(dir_file=dir_yolo_version_v)
-else:
-    yolo_v = utilities.download_yolo(name=name_yolo_version_v, dir_file=dir_yolo_version_v)
+yolo_v = utilities.load_yolo(dir_model=dir_yolo_version_v)
 
 box_drawer = utilities.BoxDrawer(names=yolo_v.model.names, colors=None, threshold=(255 * .3))
 
 camera_index = 1
 
-quit_key_str = 'q'
-quit_key_int = ord(quit_key_str)
+size = 1289, 720
+fps = 30
 
-timeout = 20  # in seconds
+quitting_key = 'q'
+timeout = None  # in secs
 
-cap = cv2.VideoCapture(index=camera_index)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-
-cap.set(3, 1289)
-cap.set(4, 720)
-
-# start the model by processing one image
-success, image = cap.read()
-results_v = yolo_v(source=image, stream=False, verbose=False)
-
-playing = True
-
-timer = cp.clock.Timer()
-
-while playing:
-
-    success, image = cap.read()
-
-    if success:
-
-        results_v = yolo_v(source=image, stream=True, verbose=False)
-        for results_vi in results_v:
-            image = box_drawer(boxes=results_vi.boxes, input_image=image, dir_out_image=None)
-
-        # results_v = yolo_v(source=image, stream=False, verbose=False)
-        # image = box_drawer(boxes=results_v[0].boxes, input_image=image, dir_out_image=None)
-
-        cv2.imshow(winname='Image', mat=image)
-
-        if (cv2.waitKey(delay=1) == quit_key_int) or (timer.get_seconds() >= timeout):
-            playing = False
-    else:
-        print("Can't receive frame (stream end?). Exiting ...")
-        playing = False
-
-cap.release()
-cv2.destroyAllWindows()
+detect_video(
+    model=yolo_v, source=camera_index, dir_out_video=None, show=True, size=size, fps=fps,
+    do_track=False, do_count=False, timeout=timeout, quit_key=quitting_key)
