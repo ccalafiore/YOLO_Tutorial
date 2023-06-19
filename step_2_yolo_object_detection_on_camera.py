eb6df47c0262a392ec9aa0fae1a27774860a8ea7
Carmelo Calafiore <44749641+ccalafiore@users.noreply.github.com>
2023-06-20 00:36:34 +0100

    update

--- step_2_yolo_object_detection_on_camera.py
+++ step_2_yolo_object_detection_on_camera.py
@@ -2,121 +2,7 @@
 # todo
 
 import os
-import math
-import time
-import keyboard
-import numpy as np
-import torch
 import utilities
-import calapy as cp
-import cv2
-
-
-def detect_video(
-        model, source, dir_out_video=None, show=False, size=None, fps=None,
-        do_track=False, do_count=False, timeout=None, quit_key=None):
-
-    if isinstance(model, str):
-        model = utilities.load_yolo(dir_model=model)
-
-    if isinstance(source, int):
-        cap = cv2.VideoCapture(index=source)
-    elif isinstance(source, str):
-        cap = cv2.VideoCapture(filename=source)
-    else:
-        raise TypeError('source')
-
-    if not cap.isOpened():
-        print("Cannot open camera")
-        exit()
-
-    if size is None:
-        size = w, h = cap.get(3), cap.get(4)
-    else:
-        if isinstance(size, int):
-            size = tuple([size, size])
-        elif isinstance(size, list):
-            size = tuple(size)
-        elif isinstance(size, tuple):
-            pass
-        elif isinstance(size, (np.ndarray, torch.Tensor)):
-            size = tuple(size.tolist())
-        else:
-            raise TypeError('size')
-
-        w, h = size
-        cap.set(3, w)
-        cap.set(4, h)
-
-    if fps is None:
-        fps = cap.get(cv2.CAP_PROP_FPS)
-    # if fps is None:
-    #     fps = math.floor(cap.get(cv2.CAP_PROP_FPS))
-    # elif isinstance(fps, int):
-    #     pass
-    # elif isinstance(fps, float):
-    #     fps = math.floor(fps)
-    # else:
-    #     raise TypeError('fps')
-
-    mspf = math.floor(1000 / fps)
-    spf = mspf / 1000
-
-    if timeout is None:
-        timeout = math.inf
-
-    if quit_key is None:
-        pass
-    elif isinstance(quit_key, (str, int)):
-        pass
-    else:
-        raise TypeError('quit_key')
-
-    if isinstance(source, int):
-        # start the model by processing one image
-        success, image = cap.read()
-        if success:
-            results = model(source=image, stream=False, verbose=False)
-
-    playing = True
-
-    timer = cp.clock.Timer()
-
-    while playing:
-
-        success, image = cap.read()
-
-        if success:
-
-            results = model(source=image, stream=False, verbose=False)
-
-            xyxy = results[0].boxes.xyxy
-            classes = results[0].boxes.cls
-            confidences = results[0].boxes.conf
-
-            image = box_drawer.draw_multi_boxes_in_single_image(
-                input_image=image, xyxy=xyxy, classes=classes, confidences=confidences, dir_out_image=None)
-
-            if show:
-                cv2.imshow(winname='Image', mat=image)
-                cv2.pollKey()
-                if keyboard.is_pressed(quit_key) or (timer.get_seconds() >= timeout):
-                    playing = False
-                else:
-                    # todo replace cv2.pollKey() with time.sleep() or cp.clock.wait()
-                    # cv2.waitKey(delay=mspf)
-                    time.sleep(spf)
-            else:
-                if keyboard.is_pressed(quit_key) or (timer.get_seconds() >= timeout):
-                    playing = False
-        else:
-            print("Can't receive frame (stream end?). Exiting ...")
-            playing = False
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-    return None
 
 
 root_script = __file__
@@ -131,16 +17,16 @@ dir_yolo_version_v = os.path.join(root_yolos, name_yolo_version_v + '.pt')
 
 yolo_v = utilities.load_yolo(dir_model=dir_yolo_version_v)
 
+camera_index = 0
+
 box_drawer = utilities.BoxDrawer(names=yolo_v.model.names, colors=None, threshold=(255 * .3))
 
-camera_index = 1
-
-size = 1289, 720
-fps = 30
+size = 1280, 720
+fps = 10
 
 quitting_key = 'q'
-timeout = None  # in secs
+timeout = 10  # in secs
 
-detect_video(
-    model=yolo_v, source=camera_index, dir_out_video=None, show=True, size=size, fps=fps,
-    do_track=False, do_count=False, timeout=timeout, quit_key=quitting_key)
+utilities.detect_video(
+    model=yolo_v, source=camera_index, box_drawer=box_drawer, dir_out_video=None, format_out_video=None,
+    show=True, size=size, fps=fps, do_track=False, do_count=False, timeout=timeout, quit_key=quitting_key)
--- utilities.py
+++ utilities.py
@@ -1,12 +1,16 @@
 
 import os
+import math
+import time
+import keyboard
 import pathlib
 import cv2
 from distinctipy import distinctipy
 import numpy as np
+import torch
 from ultralytics import YOLO
 import calapy as cp
-import torch
+
 
 
 def load_yolo(dir_model):
@@ -300,3 +304,156 @@ class BoxDrawer:
             return np.asarray(out_images)
 
     __call__ = draw_multi_boxes_in_multi_images
+
+
+def detect_video(
+        model, source, box_drawer=None, dir_out_video=None, format_out_video=None, show=False,
+        size=None, fps=None, do_track=False, do_count=False, timeout=None, quit_key=None):
+
+    if isinstance(model, str):
+        model = load_yolo(dir_model=model)
+
+    if box_drawer is None:
+        box_drawer = BoxDrawer(names=model.model.names, colors=None, threshold=(255 * .3))
+
+    if isinstance(format_out_video, str) and format_out_video[0] == '.':
+        format_out_video = format_out_video[slice(1, None, 1)]
+
+    if isinstance(source, int):
+        cap = cv2.VideoCapture(index=source)
+        if dir_out_video is None:
+            if format_out_video is None:
+                dir_out_video = 'output_video.mp4'
+                format_out_video = 'mp4'
+            else:
+                dir_out_video = '.'.join(['output_video', format_out_video])
+        else:
+            path_out_video = pathlib.Path(dir_out_video)
+            if format_out_video is None:
+                if len(path_out_video.suffix) == 0:
+                    format_out_video = 'mp4'
+                    dir_out_video = '.'.join([dir_out_video, format_out_video])
+                else:
+                    format_out_video = path_out_video.suffix[slice(1, None, 1)]
+
+    elif isinstance(source, str):
+        cap = cv2.VideoCapture(filename=source)
+        path_source = pathlib.Path(source)
+        if dir_out_video is None:
+            if format_out_video is None:
+                dir_out_video = os.path.join(os.getcwd(), 'output_' + path_source.name)
+                format_out_video = path_source.suffix[slice(1, None, 1)]
+            else:
+                dir_out_video = os.path.join(os.getcwd(), 'output_' + '.'.join([path_source.stem, format_out_video]))
+        else:
+            path_out_video = pathlib.Path(dir_out_video)
+            if format_out_video is None:
+                if len(path_out_video.suffix) == 0:
+                    format_out_video = path_source.suffix[slice(1, None, 1)]
+                    dir_out_video = '.'.join([dir_out_video, format_out_video])
+                else:
+                    format_out_video = path_out_video.suffix[slice(1, None, 1)]
+    else:
+        raise TypeError('source')
+
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    if size is None:
+        size = w, h = cap.get(3), cap.get(4)
+    else:
+        if isinstance(size, int):
+            size = tuple([size, size])
+        elif isinstance(size, list):
+            size = tuple(size)
+        elif isinstance(size, tuple):
+            pass
+        elif isinstance(size, (np.ndarray, torch.Tensor)):
+            size = tuple(size.tolist())
+        else:
+            raise TypeError('size')
+
+        w, h = size
+        cap.set(3, w)
+        cap.set(4, h)
+
+    if fps is None:
+        fps = cap.get(cv2.CAP_PROP_FPS)
+    # if fps is None:
+    #     fps = math.floor(cap.get(cv2.CAP_PROP_FPS))
+    # elif isinstance(fps, int):
+    #     pass
+    # elif isinstance(fps, float):
+    #     fps = math.floor(fps)
+    # else:
+    #     raise TypeError('fps')
+
+    mspf = math.floor(1000 / fps)
+    spf = mspf / 1000
+
+    if timeout is None:
+        timeout = math.inf
+
+    if quit_key is None:
+        pass
+    elif isinstance(quit_key, (str, int)):
+        pass
+    else:
+        raise TypeError('quit_key')
+
+    if format_out_video == 'mp4':
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    else:
+        raise ValueError('format_out_video')
+
+    video_writer = cv2.VideoWriter(dir_out_video, fourcc, fps, (w, h), 1)
+
+    if isinstance(source, int):
+        # start the model by processing one image
+        success, image = cap.read()
+        if success:
+            results = model(source=image, stream=False, verbose=False)
+
+    playing = True
+
+    timer = cp.clock.Timer()
+
+    while playing:
+
+        success, image = cap.read()
+
+        if success:
+
+            results = model(source=image, stream=False, verbose=False)
+
+            xyxy = results[0].boxes.xyxy
+            classes = results[0].boxes.cls
+            confidences = results[0].boxes.conf
+
+            image = box_drawer.draw_multi_boxes_in_single_image(
+                input_image=image, xyxy=xyxy, classes=classes, confidences=confidences, dir_out_image=None)
+
+            video_writer.write(image)
+
+            if show:
+                cv2.imshow(winname='Image', mat=image)
+                cv2.pollKey()
+                if keyboard.is_pressed(quit_key) or (timer.get_seconds() >= timeout):
+                    playing = False
+                    cv2.destroyAllWindows()
+                else:
+                    # todo replace cv2.pollKey() with time.sleep() or cp.clock.wait()
+                    # cv2.waitKey(delay=mspf)
+                    time.sleep(spf)
+            else:
+                if keyboard.is_pressed(quit_key) or (timer.get_seconds() >= timeout):
+                    playing = False
+        else:
+            print("Can't receive frame (stream end?). Exiting ...")
+            playing = False
+
+    cap.release()
+    video_writer.release()
+
+    return None
